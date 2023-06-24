6cebd8f13a4e279f25a2e98a9d5654ca1c16e194
Edwin-Mwangi <edwinmwangi138@gmail.com>
2023-06-24 18:02:21 +0300

    react basics, dynamic values etc

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  //using dynamic values
+  const title = 'Welcome to a new blog';
+  const likes = 50;
+  const person = { name: "Yoshi", age: 35}
+  const link = 'https://www.google.com'
+
   return (
+    // jsx code, similar to HTML...ie check className
+    // added js jsreact to emmet for shortcuts to work
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className="content">
+        {/* dynamic vals used on jsx */}
+        <h1>{ title }</h1>
+        {/* <p>{ person }</p> error Objs not valid react child */}
+        <p>{ person.name } Liked { likes} times</p>
+        <p>{ [1,2,3,4,5]} 
+          <br /><span>{ Math.random() * 10 }</span> 
+          <a href={ link }> Google site</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
