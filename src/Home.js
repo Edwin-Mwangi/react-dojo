cc72d9a6df2fb4e7458b7ad21b940afdfb2c7a7e
Edwin-Mwangi <edwinmwangi138@gmail.com>
2023-07-02 22:36:32 +0300

    set loading message

--- src/Home.js
+++ src/Home.js
@@ -6,10 +6,9 @@ import Bloglist from "./Bloglist";
 const Home = () => {
     const [ blogs, setBlogs] = useState(null);
 
-    const [ name, setName ] = useState('mario')
+    //to set loading...
+    const [isPending, setIsPending ] = useState(true)
 
-    //rans when render happens
-    //can specify only which renders it shouls ran using array as 2nd arg
     //[] empty array for only 1st load
     useEffect(() => {
         fetch()
@@ -22,36 +21,26 @@ const Home = () => {
             })
     }, [])
 
-    //passed as prop and called in Bloglist.js
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
+    //unnessary as data now called from server
+
+    // const handleDelete = (id) => {
+    //     const newBlogs = blogs.filter(blog => blog.id !== id);
+    //     setBlogs(newBlogs);
+    // }
  
     return ( 
         <div className="home">
+            {/* loading txt... */}
+            { isPending && <div>Loading ...</div> } 
+
             {/* props defined */}
             {/* bloglist has error as fetch is async(takes time) so blogs still null */} 
             {/* to solve error use conditional templating ie {blogs && ...} */}
-            { blogs && < Bloglist blogs = { blogs } title = 'All blogs' handleDelete = { handleDelete }/> }
+            { blogs && < Bloglist blogs = { blogs } title = 'All blogs' /> }
             
             {/* filtered based on author */}
-            { blogs && <Bloglist blogs = { blogs.filter(blog => blog.author === 'mario') } handleDelete = { handleDelete } title = "Mario's blogs"/>}
+            { blogs && <Bloglist blogs = { blogs.filter(blog => blog.author === 'mario') } title = "Mario's blogs"/>}
 
-               
-            {/* changing name to show useEffect */}
-            {/* only rans when u change name */}
-            <button onClick={ () => setName('luigi')}>Change name</button>
-            <p>{ name }</p>
-
-            {/* redudant code below */}
-            {/* {blogs.map((blog) => ( //jsx goes inside () instead 0f {}
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.title }</h2>
-                    <p>Written by { blog.author }</p>
-                </div>
-            )
-            )} */}
         </div>
      );
 }
