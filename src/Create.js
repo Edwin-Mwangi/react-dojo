f292cba027da2ecb9cf8d51479552acac9746a3c
Edwin-Mwangi <edwinmwangi138@gmail.com>
2023-07-05 15:18:27 +0300

    forma and handling form inputs -2way data binding

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,39 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState('')
+    const [body, setBody] = useState('')
+    const [author, setAuthor] = useState('mario')
+
     return ( 
-        <div>
-            <h2>Create</h2>
+        <div className="create">
+            <h2>Add a New Blog</h2>
+            <form>
+                <label>Blog Title</label>
+                <input 
+                    type="text"
+                    required
+                    // 2 way data binding
+                    value={ title }
+                    onChange={ (e) => setTitle(e.target.value)}
+                 />
+                 <label>Blog Body</label>
+                <textarea 
+                    type="text"
+                    required
+                    value={ body }
+                    onChange={ (e) => setBody(e.target.value)}
+                 />
+                 <label>Blog Author</label>
+                 <select>
+                    value={ author }
+                    onChange={ (e) => setAuthor(e.target.value)}
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                 </select>
+                <button>Add blog</button>
+                <p>{ title}</p>
+            </form>
         </div>
      );
 }
