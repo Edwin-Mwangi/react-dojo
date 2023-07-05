e86eec92a60b72be9c88c0f32997abf6e83cae4d
Edwin-Mwangi <edwinmwangi138@gmail.com>
2023-07-05 15:37:25 +0300

    delete individual blogs

--- src/blogDetails.js
+++ src/blogDetails.js
@@ -1,9 +1,18 @@
-import { useParams } from "react-router-dom/cjs/react-router-dom";
+import { useHistory, useParams } from "react-router-dom/cjs/react-router-dom";
 import useFetch from "./useFetch";
+
 
 const BlogDetails = () => {
     const { id } = useParams();
     const {data :blog, isPending, error} = useFetch("http://localhost:8000/blogs/" + id)
+    const history = useHistory();
+
+    //deleting blog
+    const handleClick = () => {
+        fetch("http://localhost:8000/blogs/" + blog.id, {
+            method: 'DELETE'
+        }).then(() => history.push('/'))
+    } 
     return ( 
         <div className="details">
             { isPending && <div>Loading...</div>} 
